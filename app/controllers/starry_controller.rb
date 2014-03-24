2c697bd72fefb1968ad071e0f8eb8ba960a9ea08
David Small <david@dazm.co>
2014-03-24 05:42:10 -0400

    Initial favorites query and model creation.

--- app/controllers/starry_controller.rb
+++ app/controllers/starry_controller.rb
@@ -9,15 +9,29 @@ class StarryController < ApplicationController
     unless session[:user_id]
       redirect_to root_url
     else
+      search_options = {
+        :count => 100
+      }
       @user = User.find_by id: session[:user_id]
-      puts ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], @user.to_json
       client = Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV['TWITTER_API_KEY']
         config.consumer_secret     = ENV['TWITTER_API_SECRET']
         config.access_token        = @user.access_token
         config.access_token_secret = @user.access_token_secret
       end
-      puts client.favorites({:max_id => 447421699105906688, :count => 1})[0].to_json
+      most_recent_favorite = FavoriteTweet.where(user: @user.id).order(date_posted: :desc).first
+      puts most_recent_favorite
+      if most_recent_favorite
+        search_options.max_id = most_recent_favorite.staus_id
+      end
+      # TODO query until all favorites retrieved
+      # TODO change query to first get most recent tweets, then tweets before
+      # the last returned favorite in each batch
+      user_favorites = client.favorites(search_options)
+      user_favorites.each do |favorite_data|
+        FavoriteTweet.create_new_favorite favorite_data, session[:user_id]
+      end
+
     end
   end
 
