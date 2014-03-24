119cf6282e6318e78bef29cc321e59a8cdafed08
David Small <david@dazm.co>
2014-03-24 04:22:59 -0400

    Prelim api connection setup.

--- app/controllers/starry_controller.rb
+++ app/controllers/starry_controller.rb
@@ -5,6 +5,22 @@ class StarryController < ApplicationController
     end
   end
 
+  def get_faves
+    unless session[:user_id]
+      redirect_to root_url
+    else
+      @user = User.find_by id: session[:user_id]
+      puts ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], @user.to_json
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['TWITTER_API_KEY']
+        config.consumer_secret     = ENV['TWITTER_API_SECRET']
+        config.access_token        = @user.access_token
+        config.access_token_secret = @user.access_token_secret
+      end
+      puts client.status(447904129692999680).to_json
+    end
+  end
+
   def signin
   end
 
