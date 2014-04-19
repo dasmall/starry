c25a52ed7179a248d8e9bf4eb35b04eca2db2ea7
Diego Salazar <diego@greyrobot.com>
2014-04-18 22:26:41 -0400

    status_id bigint

--- db/migrate/20140419022158_change_status_id_column_in_favorite_tweet.rb
+++ db/migrate/20140419022158_change_status_id_column_in_favorite_tweet.rb
@@ -1,6 +1,6 @@
 class ChangeStatusIdColumnInFavoriteTweet < ActiveRecord::Migration
   def change
     remove_column :favorite_tweets, :status_id
-    add_column :favorite_tweets, :status_id, :integer
+    add_column :favorite_tweets, :status_id, :integer, limit: 8
   end
 end
