a4427c9432029746f76ebe01104a00bd61a826ea
Diego Salazar <diego@greyrobot.com>
2014-04-18 22:28:54 -0400

    giving type in remove_column

--- db/migrate/20140419022158_change_status_id_column_in_favorite_tweet.rb
+++ db/migrate/20140419022158_change_status_id_column_in_favorite_tweet.rb
@@ -1,6 +1,6 @@
 class ChangeStatusIdColumnInFavoriteTweet < ActiveRecord::Migration
   def change
-    remove_column :favorite_tweets, :status_id
+    remove_column :favorite_tweets, :status_id, :integer
     add_column :favorite_tweets, :status_id, :integer, limit: 8
   end
 end
